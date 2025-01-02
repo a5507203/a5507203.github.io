251cf74c64ae457dda813424f849ccaa7d7e5bc6
a5507203 <yaoyu5507203@gmail.com>
2025-01-02 21:00:42 +1100

    Update news.js

--- news.js
+++ news.js
@@ -6,7 +6,7 @@ const newsItems = [
     { date: '[08-2024]', text: "I am an Area Chair for ICLR'25." },
     { date: '[06-2024]', text: "I am organizing Workshop on Trustworthy and Responsible AI for CIKM'24." },
     { date: '[02-2024]', text: "I am an Action Editor for TMLR." },
-    { date: '[01-2024]', text: "One papers is accepted by ICLR'24 (Spotlight). Congrats Ziming Hong!" },
+    { date: '[01-2024]', text: "One paper is accepted by ICLR'24 (Spotlight). Congrats Ziming Hong!" },
     { date: '[01-2024]', text: "One paper is accepted by ICLR'24. Congrats Jiyang Zheng!" },
     { date: '[12-2023]', text: "I joined The University of Sydney as a Lecturer." },
     { date: '[09-2023]', text: "One paper is accepted by NeurIPS'23." },
